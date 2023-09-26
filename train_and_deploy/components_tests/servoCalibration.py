7e80f473ed1db72af0e89a799a59558b417df3fd
jmg04006 <jmg04006@cub.uca.edu>
2023-09-26 15:49:24 -0500

    7

--- train_and_deploy/components_tests/servoCalibration.py
+++ train_and_deploy/components_tests/servoCalibration.py
@@ -1,15 +1,15 @@
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(17, min_angle=-100, max_angle=100)
+servo = AngularServo(17, min_angle=-130, max_angle=130)
 
-i = -100
+i = -130
 
 while True:
     servo.angle = i
     print (i)
     sleep(3)
     i += 5
-    if (i > 100):
+    if (i > 130):
         break
 
