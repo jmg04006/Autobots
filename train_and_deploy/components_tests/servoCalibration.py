ef5f16c4c6e0998ec99ada4aaf97a5b273ec2fa0
jmg04006 <jmg04006@cub.uca.edu>
2023-09-26 15:43:53 -0500

    6

--- train_and_deploy/components_tests/servoCalibration.py
+++ train_and_deploy/components_tests/servoCalibration.py
@@ -1,7 +1,7 @@
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(17, min_angle=-90, max_angle=90)
+servo = AngularServo(17, min_angle=-100, max_angle=100)
 
 while True:
     i = -100
