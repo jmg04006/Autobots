878c63255f746339fcba2a32e7524487fdac725c
jmg04006 <jmg04006@cub.uca.edu>
2023-10-17 16:56:18 -0500

    5

--- train_and_deploy/components_tests/motor_servo_joystick.py
+++ train_and_deploy/components_tests/motor_servo_joystick.py
@@ -2,6 +2,8 @@ import pygame
 from pygame.locals import *
 from pygame import event, display, joystick
 from gpiozero import PhaseEnableMotor
+from gpiozero import AngularServo
+
 
 is_lifted = input("Is any tire having contact with the ground or other objects? [yes/no]")
 assert is_lifted=="no"
@@ -19,14 +21,19 @@ pygame.display.init()
 pygame.joystick.init()
 
 motor = PhaseEnableMotor(phase=19, enable=26)
+servo = AngularServo(17, min_angle=-90, max_angle=90)
+
 
 throttle = 0
+steer = 0
 try:
     while(True):
         for e in pygame.event.get():
             if e.type == pygame.JOYAXISMOTION:
                 throttle = -js.get_axis(1)  # throttle input: -1: max forward, 1: max backward
-                print(throttle)
+                steer = -js.get_axis(3)  # steer_input: -1: left, 1: right
+
+                print(steer)
                 if (throttle < 0.05 and throttle > -0.05):
                     motor.stop()
                 elif (throttle > 0.05):
